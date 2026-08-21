0d4ff226104d7dc09c6aee1fbe4ac0e11d6de29a
bjfg62603-sketch <bjfg62603@gmail.com>
2026-08-21 17:57:21 +0500

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-BOT_TOKEN = "8799739281:AAGeD8cWh2GKey6M-zXH-7q9yAsieZz0I_c"
-PAYMENT_PROVIDER_TOKEN = os.getenv("PAYMENT_PROVIDER_TOKEN")  # получи у @BotFather
-ADMIN_IDS = [8428048355, 8164031956]  # @oryke и @shezik
-BASE_URL = os.getenv("RENDER_URL")  # или захардкодь: "https://твой-бот.onrender.com"
-WEBHOOK_PATH = f"/webhook/{BOT_TOKEN}"
